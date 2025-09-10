763f635921a7190872a4ff414b11109a14688473
Vishnu Vardhan Vankayalapati <vankayalapativv.dev@gmail.com>
2025-09-10 13:18:42 +0530

    Day-1

---
+++ Day-1/Introduction-to-Javascript/introduction.js
@@ -0,0 +1,41 @@
+let abhi = 10;
+
+abhi = "vishnu";
+
+// console.log(abhi);
+
+// compiled at run-time
+
+// Variable Naming Rules:
+
+// const vishnuVardhan = 10;
+// const _abhi = 20;
+// const $bharath = 10;
+// const srikanth_reddy = 10;
+
+// const 2naga = 2;
+// const jaya_dev = 20;
+
+// Variable Scopes:
+
+var vishnu = 2020; // mutable --> existing ga vunna value ni update cheyatam.
+
+//immutable --> already existing ga vunna value ni update cheyalemu.
+
+vishnu = "vardhan";
+
+// console.log(vishnu);
+
+const bharath = 2025; // immutable--> can't change the value.
+
+// bharath = "baba"; --> Error
+
+// console.log(bharath)
+
+let srikanth = "2021"; // mutable
+
+srikanth = "Srikanth Reddy";
+
+// console.log(srikanth)
+
+
