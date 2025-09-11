3dad3a78fd529ca3ce7eb2d6333e1104710974eb
Vishnu Vardhan Vankayalapati <vankayalapativv.dev@gmail.com>
2025-09-11 13:08:39 +0530

    data types

---
+++ Day-2/Alli-about-Variables/dataTypes.js
@@ -0,0 +1,45 @@
+// Data Types: Values, That can be stored and manipulated within a program.
+
+// types of Data types:
+
+// 1. Primitive Data Types: Single & Simple Valued Data Types.
+
+// Number: It represents both integers and floting point numbers.  // Integer : - infinity to + infinity including zero // Floting point numbers: 1.1, -1.1 
+
+// String: Represents of sequences of characters. or Collection of characters, enclosed in single or double quotes, Character can be a Number, Text or special characters. // Examples: let abhi = "10";, let vishnu = "punju"; let srikanth = "ep2020@"
+
+// Boolean: Represents of logical entities. True or False; // Example: // let abhi = false; // abhi = true;
+
+// Undefined: // let vishnu; // as of now, vishnu is undefined. variable vishnu is created, but value never assigned! // vishnu = "Vishnu Vardhan" // console.log(vishnu);
+
+// Null: Null is a value, but nothing just 'NULL' // let abhi = null; // console.log(abhi);
+
+// 2. Non- Primitive Data Type: Complex data types, Objects(JSON)
+
+    // let user = {
+    //     first_name : "Abhi",
+    //     last_name : "Kanu"
+    // }
+
+    // console.log(user.first_name + " " + user.last_name);
+
+// typeof()
+    
+
+
+    let vishnu = true ;
+
+  
+    console.log(typeof(vishnu));
+    
+
+
+
+
+
+
+
+
+
+
+
